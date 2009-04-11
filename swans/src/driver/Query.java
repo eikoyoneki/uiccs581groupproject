0470cce11b9ba09f41423b2c3fc7e80af465cbc2
Wenxuan.Gao <Wenxuan.Gao@fd73678c-254f-11de-872d-f12990f884b8>
2009-04-11 02:10:20 +0000

    Query implementation

--- swans/src/driver/Query.java
+++ swans/src/driver/Query.java
@@ -1,5 +1,58 @@
 package driver;
 
 public class Query {
-
+	
+	double center;
+	double range;
+	
+	public Query()
+	{
+		this.center = 0.5;
+		this.range =0.1;		
+	}
+	
+	public Query(Query q)
+	{
+		this.center = q.center;
+		this.range = q.range;		
+	}
+	public Query(double center, double range) {
+		//super();
+		//check the boundary condition here
+		if(center <=0 || center >=1)
+			{
+			System.out.println("center should be between 0 and 1");
+			System.exit(1);
+			}
+		if(range<=0||range>=center || (center+range)>=1)
+		{
+			System.out.println("range must be positive and less than center");
+			System.exit(1);
+		}
+		this.center = center;
+		this.range = range;
+	}
+		
+	public double getCenter() {
+		return this.center;
+	}
+	
+	public double getRange() {
+		return range;
+	}
+	
+	public void setQuery(double center,double range) {
+		if(center <=0 || center >=1)
+		{
+		System.out.println("center should be between 0 and 1");
+		System.exit(1);
+		}
+	if(range<=0||range>=center || (center+range)>=1)
+	{
+		System.out.println("range must be positive and less than center");
+		System.exit(1);
+	}
+	this.center = center;
+	this.range = range;
+	}
 }

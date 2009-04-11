357e95cc9021a87d4b392aa7be1906b3fe5ecea7
Wenxuan.Gao <Wenxuan.Gao@fd73678c-254f-11de-872d-f12990f884b8>
2009-04-11 06:55:20 +0000

    re-organized as item & book, so that book can be used in a more convenient way

---
+++ swans/src/driver/ReportBook.java
@@ -0,0 +1,94 @@
+//Wenxuan Gao, spring 2009
+package driver;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class ReportBook {
+	static private long gRN = 0;//global report number control
+	private Vector<ReportItem> ReportList;
+	
+	public ReportBook(){
+		ReportList = new Vector<ReportItem>();
+	}
+	public ReportBook(ReportBook book){
+		gRN = book.gRN;
+		ReportList = book.getReportList();
+	}
+	public static long getGRN() {
+		return gRN;
+	}
+	public static void setGRN(long grn) {
+		gRN = grn;
+	}
+	public Vector<ReportItem> getReportList() {
+		return ReportList;
+	}
+	public void setReportList(Vector<ReportItem> reportList) {
+		ReportList = reportList;
+	}
+	
+	synchronized public long addReport(int node, int size, double value){
+		//add a new report
+		//Every new report have to be added using this method
+		//to ensure its id is unique
+		ReportItem report = new ReportItem();
+		report.setReport_id(++gRN);
+		report.setHome_node(node);
+		report.setSize(size);
+		report.setValue(value);
+		return addReport(report);
+	}
+	synchronized public long addReport(ReportItem report) {
+		//add a existing report
+		this.getReportList().add(report);				
+		return gRN;
+	}
+	
+	synchronized public boolean delReport(long r_id ){
+		//delete an report
+		ReportItem reportItem = null;
+		Iterator<ReportItem> it=null;
+		it = this.getReportList().iterator();
+		while(it.hasNext())
+		{
+			reportItem =  it.next();
+
+			if(reportItem.getReport_id() == r_id)
+			{
+				//this is the order we should remove
+				it.remove();
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	synchronized public boolean delReport(ReportItem r){
+		long rid= r.getReport_id();
+		return delReport(rid);		
+	}
+	
+	public boolean isReportExisting(long r_id) {
+		// is report already in the book?
+		ReportItem reportItem = null;
+		Iterator<ReportItem> it=null;
+		it = this.getReportList().iterator();
+		while(it.hasNext())
+		{
+			reportItem =  it.next();
+
+			if(reportItem.getReport_id() == r_id)
+			{
+				return true;				
+			}
+		}		
+		return false;
+	}
+	
+	public boolean isReportExisting(ReportItem r) {
+		long rid=r.getReport_id();
+		return isReportExisting(rid);
+	}
+
+}

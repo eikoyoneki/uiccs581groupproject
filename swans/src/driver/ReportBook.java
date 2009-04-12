ebbadddac74f66688949d92fee61255276a9e807
dingxwsimon <dingxwsimon@fd73678c-254f-11de-872d-f12990f884b8>
2009-04-12 21:57:52 +0000

    implement LRU,LFU

--- swans/src/driver/ReportBook.java
+++ swans/src/driver/ReportBook.java
@@ -1,6 +1,7 @@
 //Wenxuan Gao, spring 2009
 package driver;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Vector;
 
@@ -8,7 +9,10 @@ public class ReportBook {
 	static private long gRN = 0;//global report number control
 	private Vector<ReportItem> ReportList;
 	private final int sizeLimit = 10;
+	private HashSet<Long> neighborReportIdList = new HashSet<Long>();
 	
+	
+
 	public ReportBook(){
 		ReportList = new Vector<ReportItem>();
 	}
@@ -16,18 +20,7 @@ public class ReportBook {
 		gRN = book.gRN;
 		ReportList = book.getReportList();
 	}
-	public static long getGRN() {
-		return gRN;
-	}
-	public static void setGRN(long grn) {
-		gRN = grn;
-	}
-	public Vector<ReportItem> getReportList() {
-		return ReportList;
-	}
-	public void setReportList(Vector<ReportItem> reportList) {
-		ReportList = reportList;
-	}
+	
 	
 	synchronized public long addReport(int node, int size, double value){
 		//add a new report
@@ -40,8 +33,9 @@ public class ReportBook {
 		report.setValue(value);
 		return addReport(report);
 	}
+	
 	synchronized public long addReport(ReportItem report) {
-		//add a existing report
+		//add a existing report received from other nodes
 		this.getReportList().add(report);				
 		return gRN;
 	}
@@ -75,14 +69,56 @@ public class ReportBook {
 				for(ReportItem otherqi : ReportList)
 				{
 					if(otherqi.getReport_id() != reportId)
+					{
 						otherqi.increaseOtherHit();
+						if(!neighborReportIdList.contains(reportId))
+							otherqi.increaseOtherHit2();
+					}
 				}
 				
 				qi.setNumOfOtherHit(0);
 				qi.increaseHit();
+				if(!neighborReportIdList.contains(reportId))
+				{
+					qi.setNumOfOtherHit2(0);
+					qi.increaseHit2();
+				}
 			}
 		}
 	}
+	
+	//create a msg body which contains the current report in the node
+	public Vector<ReportItem> createMsg(int size)
+	{
+		Vector<ReportItem> reporttoSend = new Vector();
+		rankReport();
+		int i = 0;
+		int reportnum = ReportList.size();
+		int currentsize = ReportList.get(i).getSize();
+		while(currentsize < size )
+		{
+			reporttoSend.add(ReportList.get(i));
+			i++;
+			if(i < reportnum)
+				currentsize += ReportList.get(i).getSize(); 
+			else
+				break;
+		}
+		return reporttoSend;
+	}
+	
+	//add the reports received from other nodes
+	//how to rank the new reports added?
+	public void addReport(Vector<ReportItem> reportSet)
+	{
+		
+	}
+	
+	public void rankReport()
+	{
+		CacheScheme.LRU1(this);
+	}
+	
 	
 	synchronized public boolean delReport(ReportItem r){
 		long rid= r.getReport_id();
@@ -109,6 +145,30 @@ public class ReportBook {
 	public boolean isReportExisting(ReportItem r) {
 		long rid=r.getReport_id();
 		return isReportExisting(rid);
+	}
+	
+	public static long getGRN() {
+		return gRN;
+	}
+	public static void setGRN(long grn) {
+		gRN = grn;
+	}
+	
+	//record down the report ids of the nodes that is communicating with itself 
+	public HashSet<Long> getNeighborReportIdList()
+	{
+		return neighborReportIdList;
+	}
+	public void setNeighborReportIdList(HashSet<Long> neighborReportIdList)
+	{
+		this.neighborReportIdList = neighborReportIdList;
+	}
+	
+	public Vector<ReportItem> getReportList() {
+		return ReportList;
+	}
+	public void setReportList(Vector<ReportItem> reportList) {
+		ReportList = reportList;
 	}
 
 }

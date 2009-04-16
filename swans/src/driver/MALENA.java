11c5905f4122c2096b1a493d2d133c6e90fdaa4c
Wenxuan.Gao <Wenxuan.Gao@fd73678c-254f-11de-872d-f12990f884b8>
2009-04-16 21:44:18 +0000

    Add support for KNN report

--- swans/src/driver/MALENA.java
+++ swans/src/driver/MALENA.java
@@ -18,6 +18,14 @@ public class MALENA
 			timeEncounteratNeighbor = report.getTimeEncounteratNeighbor();
 			this.fresh = fresh;
 		}
+		
+		public SupplyIV(KNNReportItem report, boolean fresh)
+		{
+			reportId = report.getReport_id();
+			age = report.getAge();
+			timeEncounteratNeighbor = report.getTimeEncounteratNeighbor();
+			this.fresh = fresh;
+		}
 	}
 	
 	private Vector<SupplyIV> trainExample = new Vector<SupplyIV>();
@@ -26,8 +34,18 @@ public class MALENA
 	{
 		
 	}
+	
+	public void bayesianTrain(KNNReportBook reportbook)
+	{
+		
+	}
 
 	public void addIV(ReportItem report, boolean fresh)
+	{
+		trainExample.add(new SupplyIV(report,fresh));
+	}
+	
+	public void addIV(KNNReportItem report, boolean fresh)
 	{
 		trainExample.add(new SupplyIV(report,fresh));
 	}

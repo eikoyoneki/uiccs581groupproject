b7e25edf2c976c7661d968dc020c0f2904742dce
Wenxuan.Gao <Wenxuan.Gao@fd73678c-254f-11de-872d-f12990f884b8>
2009-04-19 19:55:33 +0000

    add support for imote data

---
+++ trunk/swans/src/driver/NodePair.java
@@ -0,0 +1,32 @@
+package driver;
+
+public class NodePair {
+	private int x;
+	private int y;
+	
+	public NodePair(int x, int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+	
+	public void setX(int x1)
+	{
+		x=x1;
+	}
+	
+	public void setY(int y1)
+	{
+		y=y1;
+	}
+}

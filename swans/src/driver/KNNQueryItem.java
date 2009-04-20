8b2c68f9f4664c7d34208afa67203e118e60edd4
Wenxuan.Gao <Wenxuan.Gao@fd73678c-254f-11de-872d-f12990f884b8>
2009-04-20 18:27:39 +0000

    change back to not extended

--- swans/src/driver/KNNQueryItem.java
+++ swans/src/driver/KNNQueryItem.java
@@ -1,18 +1,28 @@
 package driver;
 
+import java.util.*;
 
-public class KNNQueryItem extends QueryItem {
-
+public class KNNQueryItem {
+	long  query_id;
+	int home_node;
 	double x;
 	double y;
 	
 	public KNNQueryItem(long query_id, int home_node, double x, double y) {
-		super(query_id, home_node);
+		super();
+		this.query_id = query_id;
+		this.home_node = home_node;
 		this.x = x;
 		this.y = y;
 	}
 	
 	
+	public int getHome_node() {
+		return home_node;
+	}
+	public void setHome_node(int home_node) {
+		this.home_node = home_node;
+	}
 
 	public double getX() {
 		return x;
@@ -31,5 +41,14 @@ public class KNNQueryItem extends QueryItem {
 		this.y = y;
 	}
 
+
+	public long getQuery_id() {
+		return query_id;
+	}
+
+
+	public void setQuery_id(long query_id) {
+		this.query_id = query_id;
+	}
 
 }
